7d39d70c40b1949ca074ec5e6fbce4153d022fb2
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-04-08 17:58:30 +0200

    initial scaffolding and installed fortify

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel-Share</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-    <h2 class="bg-red-600">Ciao</h2>
-</body>
-</html>
+<x-layout title="Welcome">
+
+
+ciao
+</x-layout>
